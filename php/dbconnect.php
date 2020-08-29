b0bc2dc8709e6227d8ccb88162ffdc99e56ca005
tommy2022 <56051474+tommy2022@users.noreply.github.com>
2020-08-29 21:57:26 +0900

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
     try {
       $url = parse_url(getenv('DATABASE_URL'));
-      $dsn = sprintf('pgsql:host=%s;dbname=%s;charset=utf8', $url['host'], substr($url['path'], 1));
+      $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
       $db = new PDO($dsn, $url['user'], $url['pass']);
     }
     catch (PDOException $e) {
