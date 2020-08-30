7949bffe6af6a3cd12ca8936312351a499dca557
tommy2022 <56051474+tommy2022@users.noreply.github.com>
2020-08-30 11:10:14 +0900

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,5 +1,6 @@
 <?php
     try {
+      echo "hello";
       $url = parse_url(getenv('DATABASE_URL'));
       echo substr($url['path']);
       $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
