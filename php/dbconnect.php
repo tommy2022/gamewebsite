1ca3b1578ef18a32de040873e395bd74d4fb5577
tommy2022 <56051474+tommy2022@users.noreply.github.com>
2020-08-30 14:49:41 +0900

    update

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,7 +1,6 @@
 <?php
     try {
       $url = parse_url(getenv('DATABASE_URL'));
-      echo "hello";
       echo substr($url['path']);
       $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
       $db = new PDO($dsn, $url['user'], $url['pass']);
